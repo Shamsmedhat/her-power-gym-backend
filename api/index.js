51a0c84bbfd96c45827b7d22f01a5d6c6a93af2b
Shams <shamsmedhat1@gmail.com>
2025-08-26 18:04:18 +0300

    fix: Try fix deploy issue 2.

--- api/index.js
+++ api/index.js
@@ -1,4 +1,2 @@
-// api/index.js
 const app = require('../app');
-// Hand off the request to the Express app (no app.listen on Vercel)
-module.exports = (req, res) => app(req, res);
+module.exports = app; // no app.listen on Vercel
